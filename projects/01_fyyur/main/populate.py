3d2722af7738c6bcd98e145123af2e53d288a254
James Smith <matthew02@users.noreply.github.com>
2020-05-28 21:16:27 -0500

    Added a venue.

--- projects/01_fyyur/main/populate.py
+++ projects/01_fyyur/main/populate.py
@@ -1,23 +1,23 @@
 from app import db, Artist, Venue, Show
 
 
-#data = {
-#    #"id": 1,
-#    "name": "The Musical Hop",
-#    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-#    "address": "1015 Folsom Street",
-#    "city": "San Francisco",
-#    "state": "CA",
-#    "phone": "123-123-1234",
-#    "website": "https://www.themusicalhop.com",
-#    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-#    "seeking_talent": True,
-#    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-#    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-#    "shows": []
-#}
-#venue = Venue(**data)
-#db.session.add(venue)
+data = {
+    #"id": 1,
+    "name": "The Musical Hop",
+    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
+    "address": "1015 Folsom Street",
+    "city": "San Francisco",
+    "state": "CA",
+    "phone": "123-123-1234",
+    "website": "https://www.themusicalhop.com",
+    "facebook_link": "https://www.facebook.com/TheMusicalHop",
+    "seeking_talent": True,
+    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
+    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+    "shows": []
+}
+venue = Venue(**data)
+db.session.add(venue)
 #"past_shows": [{
 #    "artist_id": 4,
 #    "artist_name": "Guns N Petals",
